003d09e27fd7421e4378f6e4eb2a0d4624e0e29d
s230267 <gimpo.s230267@ggh.goe.go.kr>
2024-11-08 14:28:21 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -12,8 +12,8 @@ with col1:
       st.subheader('모델 설명 ')
       st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
       st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
-      st.write(' - 훈련    데이터 : *건')
-      st.write(' - 테스트 데이터 : *건')
+      st.write(' - 훈련    데이터 : 21건')
+      st.write(' - 테스트 데이터 : 9건')
       st.write(' - 모델 정확도 : 0.89')
 # 3.데이터시각화
 with col2:
@@ -21,7 +21,7 @@ with col2:
       st.image('시각화1.png' )   # 이미지 불러오기
 with col3:
       st.subheader('데이터시각화2')
-      st.image('시각화2.ㅔㅜㅎ')    # 이미지 불러오기
+      st.image('시각화2.png')    # 이미지 불러오기
 
 # 4. 모델 활용
 st.subheader('모델 활용')
